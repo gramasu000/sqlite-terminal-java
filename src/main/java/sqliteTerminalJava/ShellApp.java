c01a3c1bd453363fa1c014b3ebc2232c67232d9d
Gautam <gramasu000@citymail.cuny.edu>
2020-04-20 09:07:09 -0400

    Slightly better exception handling - so that program loop will still go even when exceptions happen

--- src/main/java/sqliteTerminalJava/ShellApp.java
+++ src/main/java/sqliteTerminalJava/ShellApp.java
@@ -1,24 +1,43 @@
 package sqliteTerminalJava;
 
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 public abstract class ShellApp {
     
-    public abstract String getGreeting();
-    
-    public abstract String getPrompt();
+    protected abstract String getPrompt();
 
-    public abstract String processInput(String input);
+    protected abstract String processInput(String input);
+
+    protected abstract void begin();
+
+    protected abstract void end();
 
     public void run() {
         Scanner stdinScanner = new Scanner(System.in);
-        System.out.println(this.getGreeting());
-        while (true) {
-            System.out.print(this.getPrompt());
-            String input = stdinScanner.nextLine();
-            String output = this.processInput(input);
-            System.out.println(output);
+        this.begin();
+        System.out.print(this.getPrompt());
+        String input = stdinScanner.nextLine();
+        String output = ""; 
+        while (!input.equals("quit")) {
+            try {
+                output = this.processInput(input);
+                System.out.println(output);
+            } catch (Exception e) {
+                output = e.toString();
+                System.out.println(output);
+            }
+
+            try {
+                System.out.print(this.getPrompt());
+                input = stdinScanner.nextLine();
+            } catch (Exception e) {
+                output = e.toString();
+                System.out.println(output);
+                break;
+            }
         }
+        this.end();
     }
 
 }
